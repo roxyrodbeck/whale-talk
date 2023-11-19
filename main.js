274fc3269e9bfc21b5bc559a280a8142297200a8
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-19 13:16:03 -0600

    Update main.js
    minor changes based on dev30 answer video

--- main.js
+++ main.js
@@ -3,22 +3,18 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 var resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-    const letters = input[i];
+    const letters = input[i].toLowerCase();
+
+    if (input[i] === 'e' || input[i] === 'u') {
+        resultArray.push(input[i]); 
+    
+}
     
     for (let j = 0; j < vowels.length; j++) {
     const whale = vowels[j];
 
         if (letters === whale) {
         resultArray.push(whale);
-
-        if (input[i] === 'e') {
-            resultArray.push(input[i]); 
-    }
-
-    if (input[i] === 'u') {
-        resultArray.push(input[i]); 
-        
-}
   
     } 
 
